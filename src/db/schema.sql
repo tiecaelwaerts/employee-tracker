40d58f1713d1de27ac96bbdda424b9cb88c4ed62
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-03-02 15:09:30 -0600

    update to schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,25 +1,47 @@
+/**
+Must added to disconnect session to this database execempt this one.. 
+Note: This script is ONLY for psql
+**/
+
+SELECT 'Database rebuild started...';
+DROP DATABASE IF EXISTS temp;
+CREATE DATABASE temp;
+
+\c temp;
+
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'course_db'
+  AND pid <> pg_backend_pid();
+
+/****/
+
+DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
-USE employee_db;
+
+\c employee_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT DEFAULT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    role_id INT NOT NULL,
+    manager_id INT
+    REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
 );
