1638c6efe9592ce9e14bdabede022ec75c491a02
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-03-02 16:26:56 -0600

    updates to package and schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,3 +1,14 @@
+SELECT 'Database rebuild started...';
+DROP DATABASE IF EXISTS temp;
+CREATE DATABASE temp;
+
+\c temp;
+
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'course_db'
+  AND pid <> pg_backend_pid();
+  
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
