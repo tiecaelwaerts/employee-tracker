e243cd0601f12f67c4eb073d32f76d6cee9409e8
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-03-02 14:54:02 -0600

    updates to index and queries, removed prompts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,253 @@
-import { mainMenu } from './src/prompts';
+const inquirer = require('inquirer');
+const db = require('./queries');
+import { connectToDb } from "./database.js";
 
-console.log("Welcome to Employee Management System");
-mainMenu();
+await connectToDb();
+
+const loadPrompts = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Delete a department',
+                'Delete a role',
+                'Delete an employee',
+                'Exit'
+            ]
+        }
+    ]);
+
+    switch (choice) {
+        case 'View all departments':
+            viewDepartments();
+            break;
+        case 'View all roles':
+            viewRoles();
+            break;
+        case 'View all employees':
+            viewEmployees();
+            break;
+        case 'Add a department':
+            addDepartment();
+            break;
+        case 'Add a role':
+            addRole();
+            break;
+        case 'Add an employee':
+            addEmployee();
+            break;
+        case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+        case 'Delete a department':
+            deleteDepartment();
+            break;
+        case 'Delete a role':
+            deleteRole();
+            break;
+        case 'Delete an employee':
+            deleteEmployee();
+            break;
+        case 'Exit':
+            process.exit(1);
+    }
+};
+
+const viewDepartments = async () => {
+    const [rows] = await db.getDepartments();
+    console.table(rows);
+    loadPrompts();
+};
+
+const viewRoles = async () => {
+    const [rows] = await db.getRoles();
+    console.table(rows);
+    loadPrompts();
+};
+
+const viewEmployees = async () => {
+    const [rows] = await db.getEmployees();
+    console.table(rows);
+    loadPrompts();
+};
+
+const addDepartment = async () => {
+    const department = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the name of the department:'
+        }
+    ]);
+
+    await db.addDepartment(department.name);
+    console.log('Department added!');
+    loadPrompts();
+};
+
+const addRole = async () => {
+    const departments = await db.getDepartments();
+    const role = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of the role:'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter the salary of the role:'
+        },
+        {
+            type: 'list',
+            name: 'department_id',
+            message: 'Select the department for the role:',
+            choices: departments.map(department => ({
+                name: department.name,
+                value: department.id
+            }))
+        }
+    ]);
+
+    await db.addRole(role.title, role.salary, role.department_id);
+    console.log('Role added!');
+    loadPrompts();
+};
+
+const addEmployee = async () => {
+    const roles = await db.getRoles();
+    const employees = await db.getEmployees();
+    const employee = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Enter the first name of the employee:'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Enter the last name of the employee:'
+        },
+        {
+            type: 'list',
+            name: 'role_id',
+            message: 'Select the role for the employee:',
+            choices: roles.map(role => ({
+                name: role.title,
+                value: role.id
+            }))
+        },
+        {
+            type: 'list',
+            name: 'manager_id',
+            message: 'Select the manager for the employee:',
+            choices: employees.map(employee => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id
+            }))
+        }
+    ]);
+
+    await db.addEmployee(employee.first_name, employee.last_name, employee.role_id, employee.manager_id);
+    console.log('Employee added!');
+    loadPrompts();
+};
+
+const updateEmployeeRole = async () => {
+    const employees = await db.getEmployees();
+    const roles = await db.getRoles();
+    const employee = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee_id',
+            message: 'Select the employee to update:',
+            choices: employees.map(employee => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id
+            }))
+        },
+        {
+            type: 'list',
+            name: 'role_id',
+            message: 'Select the new role for the employee:',
+            choices: roles.map(role => ({
+                name: role.title,
+                value: role.id
+            }))
+        }
+    ]);
+
+    await db.updateEmployeeRole(employee.employee_id, employee.role_id);
+    console.log('Employee role updated!');
+    loadPrompts();
+};
+
+const deleteDepartment = async () => {
+    const departments = await db.getDepartments();
+    const department = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Select the department to delete:',
+            choices: departments.map(department => ({
+                name: department.name,
+                value: department.id
+            }))
+        }
+    ]);
+
+    await db.deleteDepartment(department.id);
+    console.log('Department deleted!');
+    loadPrompts();
+};
+
+const deleteRole = async () => {
+    const roles = await db.getRoles();
+    const role = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Select the role to delete:',
+            choices: roles.map(role => ({
+                name: role.title,
+                value: role.id
+            }))
+        }
+    ]);
+
+    await db.deleteRole(role.id);
+    console.log('Role deleted!');
+    loadPrompts();
+};
+
+const deleteEmployee = async () => {
+    const employees = await db.getEmployees();
+    const employee = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Select the employee to delete:',
+            choices: employees.map(employee => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id
+            }))
+        }
+    ]);
+
+    await db.deleteEmployee(employee.id);
+    console.log('Employee deleted!');
+    loadPrompts();
+};
+
+
+
+loadPrompts();
--- src/queries.js
+++ src/queries.js
@@ -1,43 +1,57 @@
-const db = require('./database');
+import { pool } from "./database.js";
 
-const getDepartments = () => {
-    return db.promise().query("SELECT * FROM department");
+const getDepartments = async () => {
+    const result = await pool.query('SELECT * FROM department');
+    return result.rows;
 };
 
-const getRoles = () => {
-    return db.promise().query(
-        `SELECT role.id, role.title, role.salary, department.name AS department
-         FROM role
-         JOIN department ON role.department_id = department.id`
-    );
+const getRoles = async () => {
+    const result = await pool.query('SELECT * FROM role');
+    return result.rows;
 };
 
-const getEmployees = () => {
-    return db.promise().query(
-        `SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, 
-        CONCAT(m.first_name, ' ', m.last_name) AS manager
-        FROM employee e
-        LEFT JOIN role ON e.role_id = role.id
-        LEFT JOIN department ON role.department_id = department.id
-        LEFT JOIN employee m ON e.manager_id = m.id`
-    );
+const getEmployees = async () => {
+    const result = await pool.query('SELECT * FROM employee');
+    return result.rows;
 };
 
-const addDepartment = (name) => {
-    return db.promise().query("INSERT INTO department (name) VALUES (?)", [name]);
+const addDepartment = async (name) => {
+    await pool.query('INSERT INTO department (name) VALUES ($1)', [name]);
 };
 
-const addRole = (title, salary, department_id) => {
-    return db.promise().query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [title, salary, department_id]);
+const addRole = async (title, salary, department_id) => {
+    await pool.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [title, salary, department_id]);
 };
 
-const addEmployee = (first_name, last_name, role_id, manager_id) => {
-    return db.promise().query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", 
-    [first_name, last_name, role_id, manager_id || null]);
+const addEmployee = async (first_name, last_name, role_id, manager_id) => {
+    await pool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)', [first_name, last_name, role_id, manager_id]);
 };
 
-const updateEmployeeRole = (employee_id, role_id) => {
-    return db.promise().query("UPDATE employee SET role_id = ? WHERE id = ?", [role_id, employee_id]);
+const updateEmployeeRole = async (employee_id, role_id) => {
+    await pool.query('UPDATE employee SET role_id = $1 WHERE id = $2', [role_id, employee_id]);
 };
 
-module.exports = { getDepartments, getRoles, getEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole };
+const deleteDepartment = async (id) => {
+    await pool.query('DELETE FROM department WHERE id = $1', [id]);
+};
+
+const deleteRole = async (id) => {
+    await pool.query('DELETE FROM role WHERE id = $1', [id]);
+};
+
+const deleteEmployee = async (id) => {
+    await pool.query('DELETE FROM employee WHERE id = $1', [id]);
+};
+
+module.exports = {
+    getDepartments,
+    getRoles,
+    getEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole,
+    deleteDepartment,
+    deleteRole,
+    deleteEmployee
+};
